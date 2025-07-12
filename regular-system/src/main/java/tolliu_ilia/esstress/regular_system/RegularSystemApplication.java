662651972f3d8c2f5d82e0748efeba4c07fc7fe6
ilia-tolliu <ilia.tolliu@gmail.com>
2025-07-13 00:07:20 +0200

    add project with regular system
    Regular system will update all accounts in a single DB transaction.
    The project is generated using start.spring.io

---
+++ regular-system/src/main/java/tolliu_ilia/esstress/regular_system/RegularSystemApplication.java
@@ -0,0 +1,13 @@
+package tolliu_ilia.esstress.regular_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegularSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegularSystemApplication.class, args);
+	}
+
+}
---
+++ regular-system/src/test/java/tolliu_ilia/esstress/regular_system/RegularSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package tolliu_ilia.esstress.regular_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegularSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
