50b6cb4b3ab97b260d33e585176d0cf167ee1299
ilia-tolliu <ilia.tolliu@gmail.com>
2025-07-13 11:59:05 +0200

    augment operations primary key
    Make sure accounts_id refers to existing account.
    Allow multiple operations on the same account within a same transaction - differ by operation name.

--- regular-system/src/main/resources/db/migration/V01.002__create-table-operations.sql
+++ regular-system/src/main/resources/db/migration/V01.002__create-table-operations.sql
@@ -1,10 +1,10 @@
 CREATE TABLE operations
 (
     operations_tx_id      UUID           NOT NULL,
-    accounts_id           UUID           NOT NULL,
+    accounts_id           UUID           NOT NULL REFERENCES accounts,
     operations_name       text           NOT NULL,
     operations_debit      NUMERIC(10, 2) NOT NULL,
     operations_credit     NUMERIC(10, 2) NOT NULL,
     operations_created_at TIMESTAMPTZ,
-    PRIMARY KEY (operations_tx_id, accounts_id)
+    PRIMARY KEY (operations_tx_id, accounts_id, operations_name)
 );
