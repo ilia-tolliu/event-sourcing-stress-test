23753ef7b0d129c25d1063f16bb67370e66c9bc4
ilia-tolliu <ilia.tolliu@gmail.com>
2025-07-13 02:23:45 +0200

    add endpoint to open account

---
+++ regular-system/src/main/java/tolliu_ilia/esstress/regular_system/rest/AccountsController.java
@@ -0,0 +1,41 @@
+package tolliu_ilia.esstress.regular_system.rest;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import tolliu_ilia.esstress.regular_system.service.AccountService;
+import tolliu_ilia.esstress.regular_system.domain.Account;
+import tolliu_ilia.esstress.regular_system.domain.NewAccount;
+
+import java.math.BigDecimal;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequestMapping("/accounts")
+@Slf4j
+@RequiredArgsConstructor
+public class AccountsController {
+
+    private final AccountService accountService;
+
+    @PostMapping(path = "")
+    @ResponseStatus(OK)
+    OpenAccountResponse openAccount(@RequestBody @Valid OpenAccountRequest json)  {
+        log.info(json.toString());
+
+        var newAccount = new NewAccount();
+        var account = accountService.openAccount(newAccount, json.amount());
+
+        return new OpenAccountResponse(account);
+    }
+
+    record OpenAccountRequest(
+            @NotNull
+            BigDecimal amount
+    ) {}
+
+    record OpenAccountResponse(Account account) {}
+}
